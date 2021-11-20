f0827cae3c7295823560614e49a180320f177935
KIRAN FAKIRA INGALE <75682892+kiraningale2001@users.noreply.github.com>
2021-11-20 09:45:21 +0530

    Update admin.py

--- contacts_app/admin.py
+++ contacts_app/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from app.models import User_table,Contact_table,Save_search,Limit_table,view_table
 # Register your models here.
+admin.site.register(User_table,Contact_table,Save_search,Limit_table,view_table)
