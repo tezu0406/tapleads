12cdd4bacbc404a199edaf916d74f7cb0c418fd0
KIRAN FAKIRA INGALE <75682892+kiraningale2001@users.noreply.github.com>
2021-11-23 10:47:00 +0530

    Add files via upload

---
+++ contacts_app/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+from django.urls.conf import include
+from contacts_app import views
+
+urlpatterns = [
+    path('',views.login, name='login_reg')
+]
